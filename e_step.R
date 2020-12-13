5ea8ac678c8759f2432c39b5c63716043dced92f
MARCTELLY <marctelly@hotmail.fr>
2020-12-13 17:58:48 +0100

    Refactor : adding of description

--- e_step.R
+++ e_step.R
@@ -1,6 +1,15 @@
 # calcul of probabilities
 
-# Conditionnal probability of continous data
+# Conditionnal probability of continous data####
+#' conditionalProbCont
+#' Takes continous observation, and return a conditional pobablilie based in the
+#' completed likehood esperance.
+#' @param continousData
+#' @param nbClass
+#' @param prop
+#' @param mu
+#' @param sigma
+#' @return tik matrix
 conditionalProbCont <- function(continousData, nbClass,prop, mu, sigma){
   require(mclust)
   n <- nrow(continousData)
@@ -15,7 +24,15 @@ conditionalProbCont <- function(continousData, nbClass,prop, mu, sigma){
   return(tik)
 }
 
-# Conditionnal probability tik of categorical data
+# Conditionnal probability tik of categorical data####
+#' conditionalProbCat
+#' Takes categorical observations and return a conditional pobablilies based in the
+#' completed likehood esperance and multinomial distribution.
+#' @param categoricalData
+#' @param nbClass
+#' @param prop
+#' @param alpha
+#' @return tik matrix
 conditionalProbCat <- function(categoricalData, nbClass, prop, alpha){
   
   tik <- matrix(NA, nrow(categoricalData), 3)
@@ -29,6 +46,19 @@ conditionalProbCat <- function(categoricalData, nbClass, prop, alpha){
   return(tik)
 }
 
+# Expectation step function 
+# ExpectationStep ####
+#' ExpectationStep
+#'  This function takes both continous and categorical observations and performs
+#'  the expectation step of EM-algorithm.
+#'  @param continousData
+#'  @param categoricalData
+#'  @param nbClass
+#'  @param prop
+#'  @param mu
+#'  @param sigma
+#'  @param alpha
+#'  @return list of tik matrix of continuous and categorical observation
 ExpectationStep <-function(continousData, categoricalData, nbClass, prop, mu, sigma, alpha){
   continousData <- continousData
   categoricalData <- categoricalData
@@ -44,13 +74,3 @@ ExpectationStep <-function(continousData, categoricalData, nbClass, prop, mu, si
               tikCategoricalData = tikCategoricalData))
 }
 
-alpha <- matrix(rnorm(4*5), ncol = 4)
-
-prop = c(0.1, 0.7, 0.2)
-mu = matrix(runif(3*4), ncol = 3)
-sigma = array(NA, dim=c(3, 4,4))
-
-for(k in 1:3)
-  sigma[k,,] = cov(matrix(runif(4*4)))
-obs = iris[,-5]
-conditionalProbCont(obs, 3, prop=prop, mu=mu, sigma= sigma)
