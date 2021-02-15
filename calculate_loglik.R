1b3169fc408d4a1f907d31073811586bc73cdc23
MonkeyDflo <florent.jakubowki@gmail.com>
2021-02-15 08:30:55 -0800

    add source file initialisation to loglik file

--- calculate_loglik.R
+++ calculate_loglik.R
@@ -51,54 +51,12 @@ CalculateLoglik = function(xCate, xConti, KnbClasse, prop, mu, sigma, alpha, ITE
   return(SommeSurIndividus)
 }
 
-# initialise ######
-# Function initialize
-initializeModel <- function(continuousData, categoricalData, nbClass, ITERMAX) {
-  require(bayess)
-  
-  if(is.factor(categoricalData)){
-    print(paste("Error categoricalData is a factor vector"))
-    return(NULL)
-  }
-  # data information
-  n <- nrow(continuousData);  p <- ncol(continuousData)
-  
-  #[Object creation] of gaussian model
-  prop <- matrix(NA, ITERMAX+1, nbClass)
-  mu <- array(NA, dim = c(ITERMAX+1, nbClass, p))
-  sigma <- array(NA, dim = c(ITERMAX+1, nbClass, p, p))
-  
-  # Creation of object that contain alpha an proportion in 
-  # multinomial model
-  nbCatLevel <- NCOL(categoricalData)
-  #alpha <- array(NA, data = c(ITERMAX, nbClass, nbCatLevel))
-  alpha <- matrix(NA, nbClass, nbCatLevel)
-  
-  ## Object initialisation
-  # gaussian model
-  prop[1,] <- rdirichlet(1, par=rep(1,nbClass))
-  mu[1,,] <- as.matrix(continuousData[sample(1:n,nbClass),])
-  for (k in 1:nbClass){
-    sigma[1,k,,] <- cov(matrix(rnorm(p*p), ncol = p))
-  }
-  
-  # Multinomial
-  alpha[,] <- rdirichlet(nbClass, par=rep(1,nbCatLevel))
-  
-  #loglik 
-  loglik <- array(NA, ITERMAX+1)
-  
-  return(list(prop = prop,
-              mu = mu,
-              sigma = sigma,
-              alpha = alpha,
-              loglik = loglik
-  ))
-}
+
 
 # tests ####
 # split ... 
 source(file = "organize_dataset.R")
+source(file = "initialisation.R")
 res = splitDatasetIntoCatAndConti(iris)
 cat = res$categoricalMat
 resCate = binarizeCateMatrix(cat)
